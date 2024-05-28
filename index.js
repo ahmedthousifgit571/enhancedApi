9a58f575a97a5a3389e3fb118d2963066c6c939b
ahmed thousif <ahmedthousif571@gmail.com>
2024-05-28 16:32:45 +0530

    stage 2

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/user_managment_system")
+mongoose.connect("mongodb://127.0.0.1:27017/enhanced_user")
 
 const express = require('express')
 const nocache = require('nocache')
 const app = express()
 app.use(nocache())
+const userRoute = require('./routes/userRoute')
+const adminRoute = require('./routes/adminRoute')
+
 
 // for user routes
-
+app.use('/',userRoute)
 
 // for admin routes
-
+app.use('/admin',adminRoute)
 
 app.listen(3000,()=>{
     console.log("Port running at http://localhost:3000/ ");
